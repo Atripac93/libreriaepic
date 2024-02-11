6f781399d5a1e05b4853b2164b150516072652a2
Carlo Atripaldi <carloatrica@gmail.com>
2024-02-11 19:21:29 +0100

    Create App.js

---
+++ libreriaepic/src/App.js
@@ -0,0 +1,15 @@
+import Navigation from "./Navbar/Navigation";
+import Welcome from "./Main/Welcome";
+import EndPage from "./Footer/EndPage";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Welcome />
+      <EndPage />
+    </div>
+  );
+}
+
+export default App;
